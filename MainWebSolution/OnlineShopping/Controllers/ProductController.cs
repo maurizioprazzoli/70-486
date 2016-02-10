92232c97124e99ef1f1e9ce389ded9755be2261f
PRAZZOLI Maurizio -AEE <Maurizio.Prazzoli@amada-engineering.eu>
2016-02-10 08:26:08 +0100

    Local commit

---
+++ MainWebSolution/OnlineShopping/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using OnlineShopping.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShopping.Controllers
+{
+    public class ProductController : Controller
+    {
+        [HttpGet]
+        public JsonResult GetDealPrice(int? productId)
+        {
+            var product = new Product { ProductId = 1, Name = "ProductName", Price = 10.40m };
+            return Json(25);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MainWebSolution/OnlineShopping/Scripts/App/Product.js
@@ -0,0 +1,25 @@
+﻿//$("h1:first").animate({ opacity: 0 });
+
+//$("h1:first").fadeIn(1000);
+
+$("h1:first").animate({ opacity: 1 });
+
+$("h1:first").fadeOut(5000);
+
+
+var productId = 1;
+
+$.ajax({
+    type: "GET",
+    datatype: "json",
+    contentType: "application/json",
+    url: "/Product/GetDealPrice/" + productId,
+    success: function (data) {
+        $(".price_ajax").html(data.d);
+    }
+});
+
+$.getJSON("/Product/GetDealPrice/" + productId,
+function (data) {
+    $(".price_getJSON").html(data.d);
+});
